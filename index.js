3b05ffd18ba0a533e91959e1ba296f0d42d1d063
AaronMoses <amoses22@uwo.ca>
2021-11-22 22:44:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,117 +1,109 @@
 const express = require('express');
-
 const app = express();
-
 const newConnection = require('./DBConnection');
-
 app.use(express.static('static'));
 
 app.get('/users', (request, response) =>{
     let connection=newConnection();
-    let userList;
     connection.connect()
 
-    connection.query('SELECT * FROM Users', (err, rows, fields)=>{
-        let content ='';
-        userList = rows;
 
-            let time1;
-            let time2;
-            let time3;
-            let time4;
-            let time5;
-            let time6;
-            let time7;
-            let time8;
-            let time9;
-            let time10;
+            let username = request.query.username;
+            let time1 = request.query.one;
+            let time2 = request.query.two;
+            let time3 = request.query.three;
+            let time4 = request.query.four;
+            let time5 = request.query.five;
+            let time6 = request.query.six
+            let time7 = request.query.seven
+            let time8 = request.query.eight
+            let time9 = request.query.nine
+            let time10 = request.query.ten
 
-
-            for (i of userList) {
-            if(i.time1 == 'undefined'){
+            //returns on  if the timeslot is checkmarked
+            //returns undefined if the checkbox is empty. 
+            if(time1 == 'undefined'){
                 time1 = 'undefined';
             } else {
                 time1 = 'on';
             }
 
-            if(i.time2 == 'undefined'){
+            if(time2 == 'undefined'){
                 time2 = 'undefined';
             } else {
                 time2 = 'on';
             }
 
-            if(i.time3 == 'undefined'){
+            if(time3 == 'undefined'){
                 time3 = 'undefinied';
             } else {
                 time3 = 'on';
             }
 
-            if(i.time4 == 'undefined'){
+            if(time4 == 'undefined'){
                 time4 = 'undefined';
             } else {
                 time4 = 'on';
             }
 
-            if(i.time5 == 'undefined'){
+            if(time5 == 'undefined'){
                 time5 = 'undefined';
             } else {
                 time5 = 'on';
             }
 
-            if(i.time6 == 'undefined'){
+            if(time6 == 'undefined'){
                 time6 = 'undefined';
             } else {
                 time6 = 'on';
             }
 
-            if(i.time7 == 'undefined'){
+            if(time7 == 'undefined'){
                 time7 = 'undefined';
             } else {
                 time7 = 'on';
             }
 
-            if(i.time8 == 'undefined'){
+            if(time8 == 'undefined'){
                 time8 = 'undefined';
             } else {
                 time8 = 'on';
             }
 
-            if(i.time9 == 'undefined'){
+            if(time9 == 'undefined'){
                 time9 = 'undefined';
             } else {
                 time9 = 'on';
             }
 
-            if(i.time10 == 'undefined'){
+            if(time10 == 'undefined'){
                 time10 = 'undefined';
             } else {
                 time10 = 'on';
             }
-
-            content += '<div>';
-            content += 'NAME : (' + username + '), TIME SLOT AVAILABILITY : (' + time1 + ' | ' + time2 + ' | ' + time3 + ' | ' + time4 + ' | ' + time5 + ' | ' + time6 + ' | ' + time7 + ' | ' + time8 + ' | ' + time9 + ' | ' + time10 + ')';
-            content += '</div>';
-        }
-        response.send(content);
-    })
-    connection.end();
-
+            conn.query(`insert into Availability values ('${username}','${time1}','${time2}','${time3}', '${time4}', '${time5}', '${time6}', '${time7}','${time8}', '${time9}', '${check10}')`
+            ,(err,rows,fields) => {
+                if(err)
+                    console.log(err);
+                else
+                    console.log('ITWORKED')
+            });
+   connection.end();
+});
 
 app.get('/input', (request, response) =>{
     let connection = newConnection();
     connection.connect();
-    connection.query(`INSERT INTO Users values('${req.query.username}', '${req.query.time1}', '${req.query.time2}', '${req.query.time3}', '${req.query.time4}', '${req.query.time5}', '${req.query.time6}', '${req.query.time7}', '${req.query.time8}', '${req.query.time9}', '${req.query.time10}')`, (err, rows, fields)=>{
+    connection.query(`INSERT INTO Users values('${request.query.username}', '${request.query.time1}', '${request.query.time2}', '${request.query.time3}', '${request.query.time4}', '${request.query.time5}', '${request.query.time6}', '${request.query.time7}', '${request.query.time8}', '${request.query.time9}', '${request.query.time10}')`, (err, rows, fields)=>{
         response.redirect('/');
     })
+    connection.end();
+});
 
-})
-
-connection.end
-})
-
+//admin login
 app.post('/login', (request, response) => {
-    let username = request.body.usr;
-    let password = request.body.pwd;
+    let username = request.body.username;
+    let password = request.body.password;
 
     let message = '';
 
